8fc60360969183b5df94ba99b2d7cd3b937a8b03
Ryan Pays <ryan.pays@gmail.com>
2020-01-16 20:28:44 +0000

    test solution

--- test.js
+++ test.js
@@ -1,1 +1,30 @@
+function setup() {
+  // Write your code here.
+  let spans = document.querySelectorAll("span");
 
+  [...spans].forEach((el, index, elArr) => {
+    el.classList.remove("active");
+
+    el.addEventListener("click", event => {
+      event.preventDefault();
+
+      let sliced = elArr.slice(index, elArr.length);
+      sliced.forEach(el => el.classList.add("active"));
+    });
+  });
+}
+
+// Example case.
+document.body.innerHTML = `
+  <div id='rating'>
+    <span class="active">*</span>
+    <span>*</span>
+    <span>*</span>
+    <span>*</span>
+    <span>*</span>
+  </div>`;
+
+setup();
+
+document.getElementsByTagName("span")[1].click();
+console.log(document.body.innerHTML);
